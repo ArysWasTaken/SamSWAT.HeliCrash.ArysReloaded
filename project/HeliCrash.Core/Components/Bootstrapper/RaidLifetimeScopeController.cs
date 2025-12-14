292453c08132007cadb765c25201a4f7f1ab8030
Arys <arys.steam@gmail.com>
2025-12-14 15:45:54 +0700

    refactor: Removed unused DisposeScope() in RaidLifetimeScopeController

--- project/HeliCrash.Core/Components/Bootstrapper/RaidLifetimeScopeController.cs
+++ project/HeliCrash.Core/Components/Bootstrapper/RaidLifetimeScopeController.cs
@@ -10,7 +10,6 @@ namespace SamSWAT.HeliCrash.ArysReloaded;
 public class RaidLifetimeScopeController
 {
     private ApplicationLifetimeScope _appLifetimeScope;
-    private LifetimeScope _raidLifetimeScope;
 
     public void Initialize(ApplicationLifetimeScope appLifetimeScope)
     {
@@ -19,20 +18,11 @@ public class RaidLifetimeScopeController
 
     public LifetimeScope CreateScope()
     {
-        _raidLifetimeScope = _appLifetimeScope.CreateChild<RaidLifetimeScope>(
+        var raidLifetimeScope = _appLifetimeScope.CreateChild<RaidLifetimeScope>(
             Singleton<GameWorld>.Instance.transform,
             childScopeName: "RaidLifetimeScope"
         );
 
-        return _raidLifetimeScope;
-    }
-
-    public void DisposeScope()
-    {
-        if (_raidLifetimeScope != null)
-        {
-            _raidLifetimeScope.Dispose();
-            _raidLifetimeScope = null;
-        }
+        return raidLifetimeScope;
     }
 }
